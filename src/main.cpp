93e8ea6d3b87107a0fc425187a0df115bca6792d
Aksenov Anton <a-akcenov@mail.ru>
2024-11-14 20:12:39 +0300

    Add necessary dynamic config components

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <userver/clients/http/component.hpp>
 #include <userver/components/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
+#include <userver/dynamic_config/client/component.hpp>
 #include <userver/dynamic_config/source.hpp>
 #include <userver/dynamic_config/storage/component.hpp>
+#include <userver/dynamic_config/updater/component.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
@@ -38,12 +40,15 @@ class DynamicConfigPrinter final : public userver::components::ComponentBase {
 }  // namespace
 
 int main(int argc, char* argv[]) {
-  auto component_list = userver::components::MinimalServerComponentList()
-                            .Append<userver::server::handlers::Ping>()
-                            .Append<userver::components::TestsuiteSupport>()
-                            .Append<userver::components::HttpClient>()
-                            .Append<userver::clients::dns::Component>()
-                            .Append<userver::server::handlers::TestsControl>()
-                            .Append<DynamicConfigPrinter>();
+  auto component_list =
+      userver::components::MinimalServerComponentList()
+          .Append<userver::components::DynamicConfigClient>()
+          .Append<userver::components::DynamicConfigClientUpdater>()
+          .Append<userver::server::handlers::Ping>()
+          .Append<userver::components::TestsuiteSupport>()
+          .Append<userver::components::HttpClient>()
+          .Append<userver::clients::dns::Component>()
+          .Append<userver::server::handlers::TestsControl>()
+          .Append<DynamicConfigPrinter>();
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
